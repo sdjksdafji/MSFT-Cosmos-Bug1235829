fd84ffe94b19a92a608ab7bd278ddfe817fa56ee
Shuyi-MSFT <sdjksdafji@gmail.com>
2014-09-02 12:29:48 -0700

    count example finished

---
+++ CosmosQueryOnBug1235829/CountEx.script.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using ScopeRuntime;
+
+
+public static class MyHelper {
+    private static Int64 GetQuantaTicks() {
+        TimeSpan quantaSpan = new TimeSpan(0, 0, 30);
+        return quantaSpan.Ticks;
+    }
+
+    private static Int64 _quantaTicks = GetQuantaTicks();
+
+    public static DateTime DateTimeToQuanta(DateTime time) {
+        Int64 quantizedTimeInTicks = (time.Ticks / _quantaTicks) * _quantaTicks;
+        return new DateTime(quantizedTimeInTicks);
+    }
+}
